677cdc813786e924a4161b9d4e5026e17ddec745
Mateusz <mateusz.ruszczynski@gmail.com>
2026-05-25 15:16:57 +0200

    feat: create users app, implement custom user model

--- library_service/settings.py
+++ library_service/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "django_filters",
     "drf_spectacular",
+    "user",
+
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.User"
