eaad3e5f6180dbbe8735b06cc8c04e4424868b32
Mateusz <mateusz.ruszczynski@gmail.com>
2026-05-25 17:13:43 +0200

    feat: implement serializer for user model

--- users/admin.py
+++ users/admin.py
@@ -8,7 +8,7 @@ User = get_user_model()
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
-    list_display = ("email", "first_name", "last_name", "is_staff")
+    list_display = ("email", "first_name", "last_name", "is_staff", "is_active")
     list_filter = ("is_staff", "is_superuser", "is_active")
     search_fields = ("email",)
     ordering = ("email",)
---
+++ users/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import get_user_model
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "id",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "is_staff",
+            "is_active",
+        )
+        read_only_fields = ("is_staff", "is_active")
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "min_length": 8,
+                "style": {"input_type": "password"},
+            }
+        }
+
+    def validate_password(self, value):
+        validate_password(value, user=self.instance)
+        return value
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
