19ef8de208f334e010394c9362115bf6a256137f
Mateusz <mateusz.ruszczynski@gmail.com>
2026-05-26 14:13:49 +0200

    feat: create borrowings views

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingReadSerializer,
+    BorrowingCreateSerializer,
+)
 
-# Create your views here.
+
+class BorrowingViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = Borrowing.objects.select_related(
+            "book",
+            "user"
+        )
+        if not self.request.user.is_staff:
+            return queryset.filter(user=self.request.user)
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return BorrowingReadSerializer
+        return BorrowingCreateSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
