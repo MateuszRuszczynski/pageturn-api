be0ad9de2c349be6035c567e2d69fa41551083f1
Mateusz <mateusz.ruszczynski@gmail.com>
2026-05-26 11:48:39 +0200

    feat: create view for the  book model

--- books/views.py
+++ books/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from books.serializers import BookSerializer
+from books.permissions import IsAdminOrReadOnly
+from books.models import Book
 
-# Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    permission_classes = IsAdminOrReadOnly
+
+    def get_queryset(self):
+        return Book.objects.all().order_by("title")
