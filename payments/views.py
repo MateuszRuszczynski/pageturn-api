6c061ce364df093521522d21b1b920d5eeb1518e
Mateusz <mateusz.ruszczynski@gmail.com>
2026-05-27 13:19:09 +0200

    feat: create a payment viewset

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+from payments.models import Payment
+from payments.serializers import PaymentSerializer
 
-# Create your views here.
+
+class PaymentViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = PaymentSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = Payment.objects.select_related("borrowing__book", "borrowing__user")
+
+        if not self.request.user.is_staff:
+            return queryset.filter(borrowing__user=self.request.user)
+
+        return queryset
