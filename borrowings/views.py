ce19a13fce18f402e8660bd30573c9f260c89db2
Mateusz <mateusz.ruszczynski@gmail.com>
2026-05-27 13:37:10 +0200

    feat: add the fine system logic

--- borrowings/views.py
+++ borrowings/views.py
@@ -10,6 +10,7 @@ from rest_framework.response import Response
 from django.db import transaction
 from datetime import date
 
+
 class BorrowingViewSet(
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
@@ -19,10 +20,7 @@ class BorrowingViewSet(
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        queryset = Borrowing.objects.select_related(
-            "book",
-            "user"
-        )
+        queryset = Borrowing.objects.select_related("book", "user")
         if not self.request.user.is_staff:
             return queryset.filter(user=self.request.user)
         return queryset
@@ -42,7 +40,7 @@ class BorrowingViewSet(
         if borrowing.actual_return_date is not None:
             return Response(
                 {"detail": "This book has already been returned."},
-                status=status.HTTP_400_BAD_REQUEST
+                status=status.HTTP_400_BAD_REQUEST,
             )
 
         with transaction.atomic():
@@ -52,6 +50,18 @@ class BorrowingViewSet(
             book = borrowing.book
             book.inventory += 1
             book.save()
+
+            if today > borrowing.expected_return_date:
+                overdue_days = (today - borrowing.expected_return_date).days
+
+                fine_amount = Decimal(overdue_days) * book.daily_fee * Decimal("2.0")
+
+                Payment.objects.create(
+                    status=Payment.StatusChoices.PENDING,
+                    type=Payment.TypeChoices.FINE,
+                    borrowing=borrowing,
+                    money_to_pay=fine_amount,
+                )
 
         serializer = self.get_serializer(borrowing)
         return Response(serializer.data, status=status.HTTP_200_OK)
