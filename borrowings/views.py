a844a185234e252c4e08ae4ec62c760a7ce2a03f
Mateusz <mateusz.ruszczynski@gmail.com>
2026-05-28 10:34:25 +0200

    feat: add the filters to the get_queryset

--- borrowings/views.py
+++ borrowings/views.py
@@ -22,8 +22,19 @@ class BorrowingViewSet(
 
     def get_queryset(self):
         queryset = Borrowing.objects.select_related("book", "user")
+
         if not self.request.user.is_staff:
             return queryset.filter(user=self.request.user)
+        else:
+            user_id = self.request.query_params.get("user_id")
+            if user_id:
+                queryset = queryset.filter(user_id=user_id)
+
+        is_active = self.request.query_params.get("is_active")
+        if is_active is not None:
+            is_active_bool = is_active.lower() in ("true", "1")
+            queryset = queryset.filter(actual_return_date__isnull=is_active_bool)
+
         return queryset
 
     def get_serializer_class(self):
